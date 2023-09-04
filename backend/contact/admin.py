8f2528290c832a953f3221b985dce0677e541495
Vladislav Boroday <vrboroday@yandex.ru>
2023-09-04 17:51:49 +0300

    Create model Contact

---
+++ backend/contact/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "phone_number",
+        "is_active",
+    )
+    search_fields = (
+        "first_name",
+        "last_name",
+        "email",
+        "phone_number",
+    )
+    list_filter = (
+        "first_name",
+        "last_name",
+        "email",
+    )
+
+
+admin.site.register(Contact, ContactAdmin)
---
+++ backend/contact/models.py
@@ -0,0 +1,66 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, AbstractUser
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from api.managers import CustomUserManager
+
+
+class Contact(AbstractBaseUser, PermissionsMixin):
+
+    first_name = models.CharField(
+        "Имя",
+        max_length=50,
+        blank=True,
+        null=True
+    )
+    last_name = models.CharField(
+        "Фамилия",
+        max_length=50,
+        blank=True,
+        null=True
+    )
+    email = models.EmailField(
+        "Электронная почта",
+        max_length=254,
+        unique=True,
+    )
+    phone_number = PhoneNumberField(
+        "Номер телефона",
+        unique=True,
+        help_text=("Формат: +99999999999"),
+    )
+    password = models.CharField(
+        "Пароль",
+        max_length=100,
+    )
+    is_staff = models.BooleanField(
+        ("Статус администратора"),
+        default=False,
+        help_text=(
+            "Определяет, может ли пользователь войти на этот сайт "
+            "с правами администратора."
+        ),
+    )
+    is_active = models.BooleanField(
+        ("Статус активности"),
+        default=True,
+        help_text=(
+            "Указывает возможность пользователя войти на портал. "
+        ),
+    )
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [
+        "password",
+        "first_name",
+        "last_name",
+        "phone_number",
+    ]
+
+    class Meta:
+        ordering = ("id",)
+        verbose_name = "Контакт"
+        verbose_name_plural = "Контакты"
+
+    def __str__(self):
+        return self.email
